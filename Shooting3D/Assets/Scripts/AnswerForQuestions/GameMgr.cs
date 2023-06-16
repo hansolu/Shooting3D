225d36fdf21f229099aabbc4b83725580bebdfa8
SBS <>
2023-06-16 10:09:54 +0900

    싱글톤 22

---
+++ Shooting3D/Assets/Scripts/AnswerForQuestions/GameMgr.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class GameMgr : MonoBehaviour
+{
+    private static GameMgr instance = null;    
+    public static GameMgr Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                //instance = new GameMgr();                
+                return null;
+            }
+            return instance;
+        }
+    }
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else //이미 instance가 존재한다면
+        {
+            Destroy(this.gameObject);
+        }        
+    }              
+
+}
